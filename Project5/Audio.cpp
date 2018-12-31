5d41f9768294075167654de7ccd205cdaabfbea3
sadegh <sadeghyazdani2079@gmail.com>
2018-12-31 12:11:24 +0330

    created Audio lib add functions to Auido.h muteAudio,pauseAudio,playAudio,loadAudio

--- Project5/Audio.cpp
+++ Project5/Audio.cpp
@@ -8,13 +8,19 @@ ALLEGRO_SAMPLE *sample = NULL;
 ALLEGRO_SAMPLE_INSTANCE *instance = NULL;
 bool isplaying = false;
 
-
+//bool isPlayingAudio()
+//{
+//	
+//}
+//LOAD Audio sample
 bool loadAuido(char addres[])
 {
 	al_init_acodec_addon();
-	printf("lo.\n");
+//	printf("lo.\n");
 	sample = al_load_sample(addres);
+	//reserve samples
 	al_reserve_samples(1);
+	//if sample is not empty
 	if (sample)
 	{
 		return true;
@@ -22,25 +28,35 @@ bool loadAuido(char addres[])
 	}
 	return false;
 }
-
+//start playing audio
 bool playAudio(ALLEGRO_PLAYMODE type)
 {
 	if (!sample)
 		return false;
+	//create an instance of sample
 	instance = al_create_sample_instance(sample);
 	if (instance )
 	{
-		//al_play_sample_instance(instance);
-		al_play_sample(sample, 1, 0, 1, type, NULL);
+		//configure properties
+		al_attach_sample_instance_to_mixer(instance, al_get_default_mixer());//attach an instance to mixer
+		al_set_sample_instance_gain(instance, 1);
+		al_set_sample_instance_pan(instance,0);
+		al_set_sample_instance_speed(instance,1);//audio play speed
+		al_set_sample_instance_playmode(instance, type);
+		al_set_sample_instance_playing(instance, true);//play audio
 	}
-	/*while (al_get_sample_instance_playing(instance))
-	{
-		printf("IS playing.\n");
-	}*/
-	
 }
-bool stopAudio()
+//pause audio
+void stopAudio()
 {
-	//al_stop_sample()
-	return 0;
+	al_set_sample_instance_playing(instance,!(al_get_sample_instance_playing(instance)));
+}
+//mute audio
+void muteAudio()
+{
+	int a = al_get_sample_instance_gain(instance);
+	if (a)
+		al_set_sample_instance_gain(instance, 0);
+	else
+		al_set_sample_instance_gain(instance, 1);
 }
--- Project5/Audio.h
+++ Project5/Audio.h
@@ -2,3 +2,5 @@
 #include<allegro5\allegro_audio.h>
 bool loadAuido(char[]);
 bool playAudio(ALLEGRO_PLAYMODE );
+void stopAudio();
+void muteAudio();
