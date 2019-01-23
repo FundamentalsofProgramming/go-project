d2a10f59ffa88a0ed8570d4d5724c29edefc3c2c
sadegh <sadeghyazdani2079@gmail.com>
2019-01-23 12:14:11 +0330

    add group lib

---
+++ Project5/Group.cpp
@@ -0,0 +1,169 @@
+#include"Group.h"
+#include<stdio.h>
+
+
+
+group _group[10][10];
+//add an elem to a group
+void addGroup(int i,int j,int token)
+{
+	//_group[i][j].i = i;
+	//_group[i][j].j = j;
+	_group[i][j].token = token;
+	
+}
+//merge two exiting group
+void addGroup(int token,int tokento)
+{
+	int temp1,temp2;
+	if (token > tokento)
+	{
+		temp1 = tokento;
+		temp2 = token;
+	}
+	else
+	{
+		temp2 = tokento;
+		temp1 = token;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (_group[i][j].token == temp2)
+			{
+				
+				_group[i][j].token = temp1;
+			}
+		}
+	}
+}
+//will create a new group (just is for preventing from conflicts with other overload
+void addGroup(int i, int j, int color,char just)
+{	
+	//_group[i][j].i = i;
+	//_group[i][j].j = j;
+	_group[i][j].token = usednumber * 10 + color + 1;
+	usednumber++;
+}
+int getColorOfGroup(int i, int j)
+{
+	return _group[i][j].token % 10 - 1;
+}
+bool checkGrouping(int i, int j, int color,int array[][10])
+{
+	int tempi[4] = { 0 }, tempj[4] = { 0 };
+	bool sw = false;
+	bool switches[4] = { 0 };
+	if (array[i][j + 1] == color)
+	{
+		tempi[0] = i;
+		tempj[0] = j+1;
+		switches[0] = true;
+		sw = true;
+	}
+	if (array[i][j -1] == color)
+	{
+		tempi[1] = i;
+		tempj[1] = j - 1;
+		switches[1] = true;
+		sw = true;
+	}
+	if (array[i+1][j ] == color)
+	{
+		tempi[2] = i+1;
+		tempj[2] = j ;
+		switches[2] = true;
+		sw = true;
+	}
+	if (array[i-1][j] == color)
+	{
+		tempi[3] = i-1;
+		tempj[3] = j;
+		switches[3] = true;
+		sw = true;
+	}
+	if (!sw)
+	{
+		addGroup(i, j, color,'1');
+		return true;
+	}
+	else
+	{
+		bool m = false;
+		for (int k = 0; k < 4; k++)
+		{
+			if (!switches[k])
+				continue;
+			for (int l = k+1; l < 4 ; l++)
+			{
+				if (!switches[l])
+					continue;
+				addGroup(i, j, _group[tempi[k]][tempj[k]].token);
+				addGroup(_group[tempi[k]][tempj[k]].token, _group[tempi[l]][tempj[l]].token);
+				m = true;
+			}
+		}
+		if (m)
+			return true;
+		else
+		{
+			for (int k = 0; k < 4 ; k++)
+			{
+				if (!switches[k])
+					continue;
+				addGroup(i, j, _group[tempi[k]][tempj[k]].token);
+				return true;
+			}
+		}
+	}
+	return false;
+}
+void deleteGroup(int i,int j,int array[][10])
+{
+	int temp = _group[i][j].token;
+	if (temp != -1) {
+		for (int k = 0; k < 10; k++)
+		{
+			for (int l = 0; l < 10; l++)
+			{
+				if (_group[k][l].token == temp)
+				{
+					_group[k][l].token = -1;
+					_group[k][l].isBlocked = false;
+					//al_destroy_bitmap();
+					array[k][l] = 0;
+					printf("%d %d has been deleted.\n",k,l);
+				}
+			}
+		}
+	}
+}
+void deleteGroup(int token)
+{
+
+}
+void printgroup()
+{
+
+}
+void setGroupBlock(int i,int j,blocktype bl,bool isbolck)
+{
+	if (bl == blocktype::ONE)
+	{
+		_group[i][j].isBlocked == isbolck;
+	}
+	else
+	{
+		for (int k = 0; k < 10; k++)
+		{
+			for (int l = 0; l < 10; l++)
+			{
+				if (_group[k][l].token == _group[i][j].token)
+				{
+					_group[k][l].isBlocked = isbolck;
+				}
+			}
+		}
+	}
+}
---
+++ Project5/Group.h
@@ -0,0 +1,23 @@
+#pragma once
+struct group
+{
+	int token=-1;//-1 mean its an empty group
+	bool isBlocked = false;
+};
+enum blocktype {
+	ALL,ONE
+};
+/*
+//will create a new group
+void addGroup(int i, int j, int color, char just );
+//merge two exiting group
+void addGroup(int token, int tokento);
+
+void addGroup(int i, int j, int token);
+*/
+int getColorOfGroup(int i, int j);
+void setGroupBlock(int i, int j, blocktype bl, bool isbolck);
+bool checkGrouping(int i, int j, int color, int array[][10]);
+void deleteGroup(int i, int j, int array[][10]);
+int static usednumber = 0;
+//
