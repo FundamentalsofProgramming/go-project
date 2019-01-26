8b49bfc1e5549bb57a8625f8abe3012d5ac83eab
sadegh <sadeghyazdani2079@gmail.com>
2019-01-26 15:11:19 +0330

    updated audio lib  to final version

--- Project5/Audio.cpp
+++ Project5/Audio.cpp
@@ -3,21 +3,29 @@
 #include<allegro5\allegro_audio.h>
 #include<allegro5\allegro_acodec.h>
 #include<allegro5\allegro.h>
-
+struct audio
+{
+	ALLEGRO_SAMPLE *sample = NULL;
+	ALLEGRO_SAMPLE_INSTANCE *instance = NULL;
+	
+};
+bool ismute = false;
 ALLEGRO_SAMPLE *sample = NULL;
 ALLEGRO_SAMPLE_INSTANCE *instance = NULL;
 bool isplaying = false;
+audio audios[6];
 
 //bool isPlayingAudio()
 //{
 //	
 //}
 //LOAD Audio sample
-bool loadAuido(char addres[])
+bool loadAuido(char addres[],Audios Au)
 {
 	al_init_acodec_addon();
 //	printf("lo.\n");
-	sample = al_load_sample(addres);
+	audios[Au].sample = al_load_sample(addres);
+	sample = audios[Au].sample;
 	//reserve samples
 	al_reserve_samples(1);
 	//if sample is not empty
@@ -28,35 +36,89 @@ bool loadAuido(char addres[])
 	}
 	return false;
 }
-//start playing audio
-bool playAudio(ALLEGRO_PLAYMODE type)
+bool loadAuido(const char addres[],Audios Au)
 {
-	if (!sample)
+	al_init_acodec_addon();
+	//	printf("lo.\n");
+	audios[Au].sample = al_load_sample(addres);
+	sample = audios[Au].sample;
+	//reserve samples
+	al_reserve_samples(1);
+	//if sample is not empty
+	if (sample)
+	{
+		return true;
+
+	}
+	return false;
+}
+//start playing audio
+bool playAudio(Audios Au,ALLEGRO_PLAYMODE type)
+{
+	switch (Au)
+	{
+	
+	case clockAu:
+		ismute = false;
+		break;
+	case Gameover:
+		ismute = false;
+		break;
+	default:
+		break;
+	}
+	if (!audios[Au].sample)
 		return false;
+	audios[Au].instance= al_create_sample_instance(audios[Au].sample);
+	instance = audios[Au].instance;
 	//create an instance of sample
-	instance = al_create_sample_instance(sample);
+	//instance = al_create_sample_instance(audios[Au].sample);
 	if (instance )
 	{
-		//configure properties
-		al_attach_sample_instance_to_mixer(instance, al_get_default_mixer());//attach an instance to mixer
-		al_set_sample_instance_gain(instance, 1);
-		al_set_sample_instance_pan(instance,0);
-		al_set_sample_instance_speed(instance,1);//audio play speed
-		al_set_sample_instance_playmode(instance, type);
-		al_set_sample_instance_playing(instance, true);//play audio
+		if (!ismute)
+		{
+			//configure properties
+			al_attach_sample_instance_to_mixer(instance, al_get_default_mixer());//attach an instance to mixer
+			al_set_sample_instance_gain(instance, 1);
+			al_set_sample_instance_pan(instance, 0);
+			al_set_sample_instance_speed(instance, 1);//audio play speed
+			al_set_sample_instance_playmode(instance, type);
+			al_set_sample_instance_playing(instance, true);//play audio
+		}
 	}
 }
 //pause audio
-void stopAudio()
+void stopAudio(Audios Au)
 {
+	instance = audios[Au].instance;
 	al_set_sample_instance_playing(instance,!(al_get_sample_instance_playing(instance)));
 }
 //mute audio
-void muteAudio()
+void muteAudio(Audios Au)
 {
+	instance = audios[Au].instance;
 	int a = al_get_sample_instance_gain(instance);
-	if (a)
-		al_set_sample_instance_gain(instance, 0);
-	else
-		al_set_sample_instance_gain(instance, 1);
+	switch (Au)
+	{
+	case clockAu:
+		break;
+	case Gameover:
+		break;
+	
+	default:
+		
+		if (a)
+		{
+			ismute = true;
+			al_set_sample_instance_gain(instance, 0);
+		}
+		else
+		{
+			ismute = false;
+			al_set_sample_instance_gain(instance, 1);
+		}
+		break;
+		break;
+	}
+	
 }
--- Project5/Audio.h
+++ Project5/Audio.h
@@ -1,6 +1,16 @@
 #pragma once
 #include<allegro5\allegro_audio.h>
-bool loadAuido(char[]);
-bool playAudio(ALLEGRO_PLAYMODE );
-void stopAudio();
-void muteAudio();
+enum Audios
+{
+	bgAu = 0, putAu=1, btnAu=2,clockAu=3,packEnd=4,Gameover=5
+};
+//load sample for instance
+bool loadAuido(char addres[], Audios Au);
+//an overload of above func
+bool loadAuido(const char addres[], Audios Au);
+//play audio instance
+bool playAudio(Audios Au, ALLEGRO_PLAYMODE type);
+//stop playing audio instance
+void stopAudio(Audios Au);
+//will mute audio
+void muteAudio(Audios Au);
